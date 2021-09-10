88c46a11cfa31a5e2141f3f9ed2c499240a562ee
yuanshulu <43362366+yuanshulu@users.noreply.github.com>
2021-09-10 10:00:55 +0800

    Add files via upload

---
+++ dist/_nuxt/08c5c07.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[16],{356:function(n,e,t){"use strict";t.r(e);var l={name:"Order"},r=t(12),component=Object(r.a)(l,(function(){var n=this.$createElement;return(this._self._c||n)("nuxt-child")}),[],!1,null,null,null);e.default=component.exports}}]);
